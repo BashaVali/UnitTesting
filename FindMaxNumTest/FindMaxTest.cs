5bc3b0ae252177e2fc9e9bb75a4f8a037cf53e98
BashaVali <bashavali222@gmail.com>
2022-11-16 04:15:45 +0530

    Test Cases

--- FindMaxNumTest/FindMaxTest.cs
+++ FindMaxNumTest/FindMaxTest.cs
@@ -4,62 +4,64 @@ namespace FindMaxNumTest
     public class FindMaxNumberTest
     {
         FindMaximumNum maxNum = new FindMaximumNum();
-        FindMaxGeneric generic= new FindMaxGeneric();
+        FindMaxGeneric<int> generic1 = new FindMaxGeneric<int>();
+        FindMaxGeneric<float> generic2 = new FindMaxGeneric<float>();
+        FindMaxGeneric<string> generic3 = new FindMaxGeneric<string>();
         [Test]
         public void GivenMaxFirstNum_WhenAnalysed_ShouldReturnFirstMax()
         {
             //AAA(Act,Arrange,Assert)
-            int result = generic.FindMax(15, 5, 10);
+            int result = generic1.FindMax(15, 5, 10);
             Assert.AreEqual(15, result);
         }
         [Test]
         public void GivenMaxSecondNum_WhenAnalysed_ShouldReturnSecondMax()
         {
-            int result = generic.FindMax(5, 15, 10);
+            int result = generic1.FindMax(5, 15, 10);
             Assert.AreEqual(15, result);
         }
         [Test]
         public void GivenMaxThirdNum_WhenAnalysed_ShouldReturnThirdMax()
         {
-            int result = generic.FindMax(10, 5, 15);
+            int result = generic1.FindMax(10, 5, 15);
             Assert.AreEqual(15, result);
         }
         [Test]
         public void GivenMaxFirstNumFloat_WhenAnalysed_ShouldReturnFirstMax()
         {
             //AAA(Act,Arrange,Assert)
-            float result = generic.FindMax(5.7F, 5.5F, 5.4F);
+            float result = generic2.FindMax(5.7F, 5.5F, 5.4F);
             Assert.AreEqual(5.7F, result);
         }
         [Test]
         public void GivenMaxSecondNumFloat_WhenAnalysed_ShouldReturnSecondMax()
         {
-            float result = generic.FindMax(5.5F, 5.7F, 5.4F);
+            float result = generic2.FindMax(5.5F, 5.7F, 5.4F);
             Assert.AreEqual(5.7F, result);
         }
         [Test]
         public void GivenMaxThirdNumFloat_WhenAnalysed_ShouldReturnThirdMax()
         {
-            float result = generic.FindMax(5.5F, 5.4F, 5.7F);
+            float result = generic2.FindMax(5.5F, 5.4F, 5.7F);
             Assert.AreEqual(5.7F, result);
         }
         [Test]
         public void GivenMaxFirstNumString_WhenAnalysed_ShouldReturnFirstMax()
         {
             //AAA(Act,Arrange,Assert)
-            string result = generic.FindMax("Peache","Apple","Banana");
+            string result = generic3.FindMax("Peache","Apple","Banana");
             Assert.AreEqual("Peache", result);
         }
         [Test]
         public void GivenMaxSecondNumString_WhenAnalysed_ShouldReturnSecondMax()
         {
-            string result = generic.FindMax( "Apple", "Peache", "Banana");
+            string result = generic3.FindMax( "Apple", "Peache", "Banana");
             Assert.AreEqual("Peache", result);
         }
         [Test]
         public void GivenMaxThirdNumString_WhenAnalysed_ShouldReturnThirdMax()
         {
-            string result = generic.FindMax( "Apple", "Banana","Peache");
+            string result = generic3.FindMax( "Apple", "Banana","Peache");
             Assert.AreEqual("Peache", result);
         }
     }
--- FindMaximumNumber/FindMaxGeneric.cs
+++ FindMaximumNumber/FindMaxGeneric.cs
@@ -2,16 +2,16 @@
 
 namespace FindMaximumNumber
 {
-    public class FindMaxGeneric
-    {
-        public T FindMax<T>(T a , T b ,T c) where T : IComparable
+        public class FindMaxGeneric<T> where T : IComparable
         {
-            if(a.CompareTo(b) > 0 && a.CompareTo(c)>0)
-                return a;
-            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
-                return b;
-            else
-                return c;
+            public T FindMax(T a, T b, T c)
+            {
+                if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
+                    return a;
+                if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
+                    return b;
+                else
+                    return c;
+            }
         }
-    }
 }
